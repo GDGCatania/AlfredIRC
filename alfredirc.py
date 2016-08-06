962c73b22c5beeb56b234c2146cc2a0a5d9f0129
Gabriele Baldoni <gabriele.baldoni@gmail.com>
2016-08-06 16:15:35 +0200

    cleaning code, formatting, add editorconfig

--- alfredirc.py
+++ alfredirc.py
@@ -13,9 +13,9 @@ import logging
 
 def connect():
 	return MySQLdb.connect(host=DBHOST,
-								user=DBUSER,
-								passwd=DBPASS,
-								db=DBNAME)
+	user=DBUSER,
+	passwd=DBPASS,
+	db=DBNAME)
 def execute_query(db,query):
 	cur = db.cursor()
 	cur.execute(query)
@@ -36,7 +36,7 @@ def send_notice(user,msg):
 
 def send_message(chan, msg):
 	logger.info('[ INFO ] Sending message %s %s...' % (chan,msg))
-    s.send(bytes('PRIVMSG %s :%s\r\n' % (chan, msg)))
+	s.send(bytes('PRIVMSG %s :%s\r\n' % (chan, msg)))
 
 def set_mode(chan,nick,mode):
 	logger.info('[ INFO ] Setting mode %s %s %s...' % (chan,nick,mode))
@@ -44,56 +44,54 @@ def set_mode(chan,nick,mode):
 
 def send_kick(chan,nick):
 	logger.info('[ INFO ] Kick user %s  from %s' % (chan,nick))
-    s.send(bytes('KICK %s %s\r\n' % (chan,nick)))
+	s.send(bytes('KICK %s %s\r\n' % (chan,nick)))
 
 def send_nick(nick):
 	logger.info('[ INFO ] Sending nick %s ... ' % nick)
-    s.send(bytes('NICK %s\r\n' % nick))
+	s.send(bytes('NICK %s\r\n' % nick))
 
 def send_pass(password):
 	logger.info('[ INFO ] Sending password %s ...' % password)
-    s.send(bytes('PASS %s\r\n' % password))
+	s.send(bytes('PASS %s\r\n' % password))
 
 def join_channel(chan):
 	logger.info('[ INFO ] Sending join %s ...' % chan)
-    s.send(bytes('JOIN %s\r\n' % chan))
-
+	s.send(bytes('JOIN %s\r\n' % chan))
 
 def part_channel(chan):
 	logger.info('[ INFO ] Sending part %s ...' % chan)
-    s.send(bytes('PART %s\r\n' % chan))
-
+	s.send(bytes('PART %s\r\n' % chan))
 
 def quit():
 	logger.info('[ INFO ] Sending quit...')
-    s.send(bytes('QUIT %s\r\n'))
+	s.send(bytes('QUIT %s\r\n'))
 
 ### HELPING 
 def get_sender(msg):
-    result = ""
-    for char in msg:
-        if char == "!":
-            break
-        if char != ":":
-            result += char
-    return result
+	result = ""
+	for char in msg:
+		if char == "!":
+			break
+		if char != ":":
+			result += char
+	return result
 
 
 def get_message(msg):
-    result = ""
-    i = 3
-    length = len(msg)
-    while i < length:
-        result += msg[i] + " "
-        i += 1
-    result = result.lstrip(':')
-    return result
+	result = ""
+	i = 3
+	length = len(msg)
+	while i < length:
+		result += msg[i] + " "
+		i += 1
+	result = result.lstrip(':')
+	return result
 
 def is_private(msg):
 	return not msg[2].startswith('#')
 
 def is_command(msg):
-    return msg.startswith('!')
+	return msg.startswith('!')
 
 def get_operators():
 	logger.info('[ INFO ] Getting operators...')
@@ -105,21 +103,19 @@ def get_operators():
 
 
 def execute_command(cmd,avaiable_commands):
-    if cmd in avaiable_commands:
-    	logger.info('[ INFO ] Executing command %s' % cmd)
-    	c=avaiable_commands[cmd]
-    	print 'output %s' % c[2]
-    	print 'subgroupid %s' % c[1]
-    	s="| "
-    	if c[2] is not None:
-    		send_message(CHAN,'%s' % c[2])
-    	for co in avaiable_commands:
-    		if avaiable_commands[co][0]==c[1]:
-    			s+="!%s | " %co
-    	if s != '| ':
-    		send_message(CHAN,s)
-    	
-    		
+	if cmd in avaiable_commands:
+		logger.info('[ INFO ] Executing command %s' % cmd)
+		c=avaiable_commands[cmd]
+		s="| "
+		if c[2] is not None:
+			send_message(CHAN,'%s' % c[2])
+		for co in avaiable_commands:
+			if avaiable_commands[co][0]==c[1]:
+				s+="!%s | " %co
+		if s != '| ':
+			send_message(CHAN,s)
+
+
 def parse_private_message(msg,user):
 	if len(msg) >= 1:
 		print ('Sender %s'  % user)
@@ -127,8 +123,7 @@ def parse_private_message(msg,user):
 		if user in operators:
 			msg = msg.split(' ')
 			options = {'!die' : die_handler,
-                        '!testdb':db_handler
-                        }                 
+			'!testdb':db_handler}
 			if msg[0] in options:
 				send_message(user,options[msg[0]]())
 		else:
@@ -141,19 +136,19 @@ def parse_private_message(msg,user):
 
 def die_handler():
 	logger.warn('[ WARN ] Received die command!')
-    send_message(CHAN, 'Bye folks')
-    part_channel(CHAN)
+	send_message(CHAN, 'Bye folks')
+	part_channel(CHAN)
 	logger.warn('[ WARN ] Shooting down!')
-    quit()
-    sys.exit(0)
+	quit()
+	sys.exit(0)
 
 
 def db_handler():
-    db=connect()
-    res=execute_query(db,'SELECT * FROM %s' % DBTABLE)
+	db=connect()
+	res=execute_query(db,'SELECT * FROM %s' % DBTABLE)
 	logger.info('[ INFO ] Testing db Result: %s' % res)
-    close_db(db)
-    return res
+	close_db(db)
+	return res
 
 #####
 
@@ -216,24 +211,19 @@ join_channel(CHAN)
 
 
 while 1:
-    readbuffer=readbuffer+s.recv(1024).decode('UTF-8')
-    temp=string.split(readbuffer, "\n")
-    readbuffer=temp.pop()
+	readbuffer=readbuffer+s.recv(1024).decode('UTF-8')
+	temp=string.split(readbuffer, "\n")
+	readbuffer=temp.pop()
 
 
-    for line in temp:
-    	print line
-        single_line=string.rstrip(line)
-       	single_line=string.split(single_line)
-        
+	for line in temp:
+		single_line=string.rstrip(line)
+		single_line=string.split(single_line)
 	if len(single_line)>=1:
-
 		if single_line[0]=="PING" :
 					logger.info('[ INFO ] Ping received!')
-       	    		send_pong(single_line[1])
-       	    		print('PONG!\n')
-
-        
+					send_pong(single_line[1])
+					print('PONG!\n')
 		if single_line[1] == 'PRIVMSG':
 			sender=get_sender(single_line[0])
 			message = get_message(single_line)
@@ -246,6 +236,3 @@ while 1:
 					cmd = message[1:].strip()
 					logger.info('[ INFO ] Message is a command: %s' % cmd)
 					execute_command(cmd,commands_map)
-
-			
-			print(sender + ": " + message)
